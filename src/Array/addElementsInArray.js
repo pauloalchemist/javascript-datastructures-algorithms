3e95edaf2751b42b1fcf4fa16ce3d2b9c7bca080
pauloalchemist <paulolinsdev@gmail.com>
2021-01-26 19:51:11 -0300

    Array com remoção a partir da posição [5], removendo três itens, usando splice

--- src/Array/addElementsInArray.js
+++ src/Array/addElementsInArray.js
@@ -1,8 +1,7 @@
 let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 numbers.push(11, 12, 13)
-
-console.log(numbers.length)
+console.log('Array com inserção de 11, 12 e 13 nas ultimas posições usando push: ', numbers)
 
 Array.prototype.insertFirstPosition = function (value) {
   for (let i = this.length; i >= 0; i--) {
@@ -19,3 +18,6 @@ console.log('Array com inserção de -2 e -3 nas primeiras posições usando uns
 
 numbers.shift()
 console.log('Array com remoção do primeiro item usando shift: ', numbers)
+
+numbers.splice(5, 3)
+console.log('Array com remoção a partir da posição [5], removendo três itens, usando splice: ', numbers)
