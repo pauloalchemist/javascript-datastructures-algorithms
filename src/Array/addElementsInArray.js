8baf21036f2daece58d12a2b74ce0f8dcee14c12
pauloalchemist <paulolinsdev@gmail.com>
2021-01-26 18:47:45 -0300

    Array com inserção de -1 na primeira posição usando prototype

--- src/Array/addElementsInArray.js
+++ src/Array/addElementsInArray.js
@@ -3,4 +3,14 @@ let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 numbers.push(11, 12, 13)
 
 console.log(numbers.length)
-console.log(numbers)
+
+Array.prototype.insertFirstPosition = function (value) {
+  for (let i = this.length; i >= 0; i--) {
+    this[i] = this[i - 1]
+  }
+  this[0] = value
+}
+
+numbers.insertFirstPosition(-1)
+
+console.log('Array com inserção de -1 na primeira posição usando prototype: ', numbers)
