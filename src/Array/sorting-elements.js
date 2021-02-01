9bc352fb5552a0f64c0574832deaf8aa4388ea6d
pauloalchemist <paulolinsdev@gmail.com>
2021-02-01 16:40:53 -0300

    array.sort()

--- src/Array/sorting-elements.js
+++ src/Array/sorting-elements.js
@@ -1,4 +1,7 @@
-
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 // inverter ordem do array
-console.log(numbers.reverse())
+const reverseNumbers = numbers.reverse()
+console.log(reverseNumbers)
+
+const sortNumbers = reverseNumbers.sort((a, b) => a - b)
+console.log(sortNumbers)
