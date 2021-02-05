e6e67da07549b52234f9d1b5d101204e13a2ce46
pauloalchemist <paulolinsdev@gmail.com>
2021-02-05 16:34:32 -0300

    StackArray tests

---
+++ tests/stacks/stack-array.spec.js
@@ -0,0 +1,27 @@
+const { describe, it, beforeEach } = require('mocha')
+const { expect } = require('chai')
+const StackArray = require('../../src/stacks/stack-array')
+
+describe('StackArray', () => {
+  let stack
+
+  beforeEach(() => {
+    stack = new StackArray()
+  })
+
+  it('starts empty', () => {
+    expect(stack.size()).to.equal(0)
+    expect(stack.isEmpty()).to.equal(true)
+  })
+
+  it('pushes elements', () => {
+    stack.push(1)
+    expect(stack.size()).to.equal(1)
+    stack.push(2)
+    expect(stack.size()).to.equal(2)
+    stack.push(3)
+    expect(stack.size()).to.equal(3)
+    console.log(stack)
+    expect(stack.isEmpty()).to.equal(false)
+  })
+})
