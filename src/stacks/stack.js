4e9b5801519c1b60015d87b925ab3794fb857486
pauloalchemist <paulolinsdev@gmail.com>
2021-02-05 18:54:12 -0300

    stack class methods

--- src/stacks/stack.js
+++ src/stacks/stack.js
@@ -8,4 +8,36 @@ module.exports = class Stack {
     this.items[this.count] = element
     this.count++
   }
+
+  size () {
+    return this.count
+  }
+
+  isEmpty () {
+    return this.count === 0
+  }
+
+  pop () {
+    if (this.isEmpty()) {
+      return undefined
+    }
+
+    this.count--
+    const result = this.items[this.count]
+    delete this.items[this.count]
+    return result
+  }
+
+  peek () {
+    if (this.isEmpty()) {
+      return undefined
+    }
+
+    return this.items[this.count - 1]
+  }
+
+  clear () {
+    this.items = {}
+    this.count = 0
+  }
 }
