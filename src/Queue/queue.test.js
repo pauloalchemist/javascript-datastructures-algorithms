02cdda01cf6c173a52b25e7eb30e81f6ebf9c55b
pauloalchemist <paulolinsdev@gmail.com>
2021-03-13 13:57:56 -0300

    add test for queue

--- src/Queue/queue.test.js
+++ src/Queue/queue.test.js
@@ -9,4 +9,28 @@ describe('teste dos métodos para queue.js', () => {
     // console.log(filaVazia)
     expect(filaVazia).to.equal(true)
   })
+
+  test('Adicionar elementos para fila', () => {
+    queue.enqueue('Paulo')
+    queue.enqueue('Vanessa')
+    queue.enqueue('Thomas')
+    const elmFila = queue.toString()
+    // console.log(elmFila)
+    expect(elmFila).to.equal('Paulo,Vanessa,Thomas')
+  })
+
+  test('Confirmar se o tamanho da fila é igual ao que foi adcionado', () => {
+    expect(queue.size()).to.equal(3)
+  })
+
+  test('Confirmar que a fila não está vazia', () => {
+    expect(queue.isEmpty()).to.equal(false)
+  })
+
+  test('Confirmar que a saída da fila siga o modelo LIFO', () => {
+    const saidaFila = queue.dequeue()
+    // console.log(saidaFila)
+    expect(saidaFila).to.equal('Paulo')
+    expect(queue.size()).to.equal(2)
+  })
 })
